f4ccab240736334082bddc5365834b261ff60f61
BizBigs <math0963@gmail.com>
2022-04-27 21:43:54 +0200

    refactor v2

--- includes/PngManager.h
+++ includes/PngManager.h
@@ -6,6 +6,13 @@
 #include <png.h>
 #include <stdlib.h>
 
-void    writePngFile(IMAGEDATA *, char*);
+typedef struct {
+    png_structp     png_ptr;
+    png_infop       info_ptr;
+} PNGSTRUCT;
+
+void            writePngFile(IMAGEDATA *, char*);
+int             initPngLib(PNGSTRUCT *);
+unsigned char   **buildPngImage(PNGSTRUCT *, IMAGEDATA *);
 
 #endif /* __PNG_MANAGER_H__*/
--- src/PngManager.c
+++ src/PngManager.c
@@ -1,47 +1,34 @@
 #include "PngManager.h"
 
-int     initPngLib(png_structp *png_ptr, png_infop *info_ptr) {
+int     initPngLib(PNGSTRUCT *pngStruct) {
      /* initialize stuff */
-    *png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    (*pngStruct).png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 
-    if (!*png_ptr) {
+    if (!(*pngStruct).png_ptr) {
         printf("[write_png_file] png_create_write_struct failed");
         return 0;
     }
 
-    *info_ptr = png_create_info_struct(*png_ptr);
-    if (!*info_ptr) {
+    (*pngStruct).info_ptr = png_create_info_struct((*pngStruct).png_ptr);
+    if (!(*pngStruct).info_ptr) {
         printf("[write_png_file] png_create_info_struct failed");
-        png_destroy_write_struct(png_ptr,  NULL);
+        png_destroy_write_struct(&(*pngStruct).png_ptr,  NULL);
         return 0;
     }
 
-    if (setjmp(png_jmpbuf(*png_ptr))) {
+    if (setjmp(png_jmpbuf((*pngStruct).png_ptr))) {
         printf("[write_png_file] Error during init_io");
-        png_destroy_write_struct(png_ptr, info_ptr);
+        png_destroy_write_struct(&(*pngStruct).png_ptr, &(*pngStruct).info_ptr);
         return 0;
     }
     return 1;
 }
 
-void    writePngFile(IMAGEDATA *imageData, char* file_name) {
-    png_structp     png_ptr;
-    png_infop       info_ptr;
-    unsigned char   **rowPointers = (unsigned char **)malloc(imageData->height * sizeof(unsigned char*));
-    /*png_bytep *rowPointers = (png_byte)malloc(imageData.height);
-    unsigned int i = 0, j = 0;*/
+unsigned char     **buildPngImage(PNGSTRUCT *pngStruct, IMAGEDATA *imageData) {
     unsigned int i = 0;
+    unsigned char   **rowPointers = (unsigned char **)malloc(imageData->height * sizeof(unsigned char*));
 
-    /* create file */
-    FILE *fp = fopen(file_name, "wb");
-    if (!fp)
-        printf("[write_png_file] File %s could not be opened for writing", file_name);
-
-    if (!initPngLib(&png_ptr, &info_ptr)) {
-        exit(EXIT_FAILURE);
-    }
-
-    png_set_IHDR(png_ptr, info_ptr, imageData->width, imageData->height, 8,
+    png_set_IHDR((*pngStruct).png_ptr, (*pngStruct).info_ptr, imageData->width, imageData->height, 8,
         PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
         PNG_COMPRESSION_TYPE_DEFAULT,
         PNG_FILTER_TYPE_DEFAULT);
@@ -50,12 +37,32 @@ void    writePngFile(IMAGEDATA *imageData, char* file_name) {
         rowPointers[i] = imageData->lpData + (i * imageData->width * 3);
         i++;
     }
+    return rowPointers;
+}
 
-    png_init_io(png_ptr, fp);
-    png_set_rows(png_ptr, info_ptr, rowPointers);
-    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-    png_destroy_write_struct(&png_ptr, &info_ptr);
+void    writePngFile(IMAGEDATA *imageData, char* file_name) {
+    PNGSTRUCT       pngStruct;
+    unsigned char   **rowPointers;
 
+    /* create file */
+    FILE *fp = fopen(file_name, "wb");
+    if (!fp)
+        printf("[write_png_file] File %s could not be opened for writing", file_name);
+
+    /* Init PNG Library */
+    if (!initPngLib(&pngStruct)) {
+        fclose(fp);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Build PNG Image from raw data */
+    rowPointers = buildPngImage(&pngStruct, imageData);
+
+    png_init_io(pngStruct.png_ptr, fp);
+    png_set_rows(pngStruct.png_ptr, pngStruct.info_ptr, rowPointers);
+    png_write_png(pngStruct.png_ptr, pngStruct.info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+    png_destroy_write_struct(&pngStruct.png_ptr, &pngStruct.info_ptr);
+    free(rowPointers);
     fclose(fp);
 }
 
